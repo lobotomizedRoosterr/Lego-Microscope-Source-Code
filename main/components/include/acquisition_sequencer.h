f42ea7d0fbdaa2784b5f827565f7cdcd2a566fdf
Austin Romney <austinromney09420@gmail.com>
2026-07-29 19:16:35 -0600

    Refine comments in acquisition_sequencer.h
    Updated comments for clarity and consistency in acquisition_sequencer.h.

--- main/components/include/acquisition_sequencer.h
+++ main/components/include/acquisition_sequencer.h
@@ -3,12 +3,13 @@
 #include "esp_err.h"
 #include <stdbool.h>
 
-#define EXPOSURE_TIME_MS 20
+#define EXPOSURE_TIME_MS 20 /*default time to wait between setting LED array and capturing an image*/
 
+/*Index of current pattern*/
 extern int current_pattern_index; 
-extern volatile bool calibration_in_progress;
 
-
+/*run the loop for camera acquisition*/
 void camera_acquisition_task(void *arg);
 
+/*initialize acquisition sequencer*/
 esp_err_t init_acquisition_sequencer();
