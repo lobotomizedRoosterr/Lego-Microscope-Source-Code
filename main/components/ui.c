4ba92e9c575dd38a80a116e638928794e03c6fc3
Austin Romney <austinromney09420@gmail.com>
2026-07-29 19:08:19 -0600

    Refactor UI button management and rendering logic
    Refactor UI component to improve button handling and rendering.

--- main/components/ui.c
+++ main/components/ui.c
@@ -23,6 +23,16 @@
 #define BUG_MODE 99
 #define DPC_GEN_MODE 98
 
+/*
+UI.c
+
+Buttons
+Every Button is stored as a struct.
+Every button has a "mode" identifier. This dictates what to do when the button is selected.
+
+*/
+
+
 // max buttons = 50
 button buttons[50];
 int button_count = 0;
@@ -54,6 +64,12 @@ void render_button(button b) {
 }
 
 esp_err_t init_ui(void) {
+
+    // Here are the definitions for every UI Button
+    //Note that the touch bounds are larger than needed. This is because many touch displays
+    //are low quality. This should ensure that buttons can be pressed
+    // see ui.h for 
+
     add_button((button) {
         .bg_color_normal=color_from_rgb(0, 0, 0),
         .fg_color_normal=color_from_rgb(255, 255, 255),
@@ -157,12 +173,25 @@ esp_err_t init_ui(void) {
 }
 void update_ui(void) {
     touch_data td = get_touch();
+
+    /*
+    TODO Change this
+    This needs to be reformatted and simplified.
+    Each button needs to contain a pointer to a function that passes values like *button self that runs when selected.
+    */
+
+    //was_tap_down makes sure that the user has to release their finger before another button register can happen.
     if(!was_tap_down && td.pressed) {
+        //Cycle through every button to update
         for(int i = 0; i < button_count; i++) {
+            //Is touch point within bounds?
             if(td.touch_x > buttons[i].t_x0 && td.touch_x < buttons[i].t_x1 && td.touch_y > buttons[i].t_y0 && td.touch_y<buttons[i].t_y1) {
+                //This is a remnant of a previous button, Debug. This is here to make sure that there are no attempts to set mode to "BUG_MODE"
                 if(buttons[i].mode == BUG_MODE) {
                     continue;
                 }
+                //The DPC button works where it gets mode, if it is a mode other than DPC it sets it to DPC LR
+                //Then, if it was DPC, it has it cycle through the different modes.
                 if(buttons[i].mode == DPC_GEN_MODE) {
                     buttons[i].is_selected=true;
                     switch(current_mode) {
@@ -191,6 +220,7 @@ void update_ui(void) {
                         buttons[i].raw_icon=dpc_rl_icon_raw;
                         break;
                     }
+                    //unselect every other button (except for i, the one that was tapped)
                     for(int j = 0; j < button_count; j++) {
                         if(j != i) {
                             buttons[j].is_selected=false;
@@ -198,20 +228,23 @@ void update_ui(void) {
                     }
                     continue;
                 }
-
+                    //unselect every other button (except for i, the one that was tapped)
                 for(int j = 0; j < button_count; j++) {
                     if(j != i) {
                         buttons[j].is_selected=false;
                     }
                 }
+                //select button
                 buttons[i].is_selected=true;
-                int mode = buttons[i].mode;
-                set_mode(mode);
+                //set mode
+                set_mode(buttons[i].mode);
 
             } 
         }
+        //make sure that the same button press isn't detected next tick.
         was_tap_down=true;
     }
+    // if no touch is detected, set was_tap_down accordingly, for the next tick
     if(was_tap_down && !td.pressed) {
         was_tap_down=false;
     }
@@ -223,12 +256,12 @@ void add_button(button b) {
 }
 
 void render_ui(void) {
-
+    //render every button
     for(int i = 0; i < button_count; i++) {
         render_button(buttons[i]);
     }
+    //render the top bar that displays mode
     fill_rect(0, 0, 288, 16, color_from_rgb(0, 0, 0));
     draw_text(1, 1, get_mode_from_id(current_mode).label, color_from_rgb(0, 255, 0));
 
-    //lcd_draw_debug_bitmap(lcd_panel_handle, 0, 0, 320, 240);
 }
