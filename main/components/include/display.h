fce201397d2cfa55fd001426107d02653f9ee0ed
Austin Romney <austinromney09420@gmail.com>
2026-07-29 19:16:03 -0600

    Enhance display.h with comments for clarity
    Added comments to clarify display dimensions and initialization function.

--- main/components/include/display.h
+++ main/components/include/display.h
@@ -4,9 +4,11 @@
 #include "esp_lcd_ili9341.h"
 #include "esp_lcd_io_spi.h"
 
-#define DISPLAY_WIDTH 320
-#define DISPLAY_HEIGHT 240
+#define DISPLAY_WIDTH 320 /*Width of display in px*/
+#define DISPLAY_HEIGHT 240 /*height of display in px*/
 
+/* handle used to identify the io configuration of the lcd*/
 extern esp_lcd_panel_io_handle_t lcd_io_handle;
 
+/*initializes display*/
 esp_err_t init_display();
